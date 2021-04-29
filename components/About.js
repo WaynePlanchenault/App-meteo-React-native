dd0112cd08863dcf56b2aed5a3deded3e9609a6b
Wayne Planchenault <planchenaultw@hotmail.fr>
2021-04-29 11:16:52 +0200

    composant about

---
+++ components/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const About = () => {
+  return (
+    // ici j'écris du JSX
+    <View>
+      <Text>A propos de moi</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor ducimus
+        quas quae numquam aspernatur similique in, tempora repellat amet sit
+        nihil nobis neque iste id ut fugit ea, cumque harum!
+      </Text>
+    </View>
+  );
+};
+
+export default About;
